8be81c7c9ab9e3820e986b7d25667ce064239ba9
mAdkins <software@doorways.org>
2019-09-01 09:54:40 -0700

    More documentation tweaks.

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 Common Go packages shared by other Go projects.
 
+These are not really production quality packages, nor are they maintained or supported.
+They're simplistic mechanisms implemented to solve specific problems
+in code that the author has been writing for his own use.
+
+*If you need this functionality consider copying the code into your own project and modifying to fit your need.*
+
+See the source for documentation.
+
 ## `path`
 
 Path utilities.
--- package.go
+++ package.go
@@ -1,6 +1,7 @@
 // Package go_utils provides simple Go utility packages.
 //
 // These are not really production quality packages, nor are they maintained or supported, they're
-// simplistic mechanisms implemented to solve specific problems in code that mAdkins23 has been writing for his own use.
+// simplistic mechanisms implemented to solve specific problems in code that the author has been writing for his own use.
+//
 // If you need this functionality consider copying the code into your own project and modifying to fit your need.
 package go_utils
