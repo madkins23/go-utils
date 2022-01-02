49961b0b30ffad7ba6ab1a5ca9fcab7c1d3402f0
mAdkins <software@doorways.org>
2022-01-02 08:30:17 -0800

    Add text.FormatUSD

---
+++ text/currency.go
@@ -0,0 +1,88 @@
+package text
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+var dollar = '$'
+var period = '.'
+var multipleDecimalPoints = errors.New("multiple decimal points")
+
+// FormatUSD takes a string representation of a number,
+// decorates it with a preceding dollar sign, and inserts commas as required.
+//
+// This is not intended to be a general purpose solution,
+// it is a stopgap until golang formalizes decimal and currency libraries.
+//
+// The current implementation is inefficient,
+// there is probably a state-machine solution
+// that checks through the number string in reverse.
+func FormatUSD(number string) (string, error) {
+	decimalFound := false
+	intPart := strings.Builder{}
+	intPart.Grow(len(number))
+	decPart := strings.Builder{}
+	decPart.Grow(len(number))
+
+	for _, c := range number {
+		if c == period {
+			if decimalFound {
+				// Can't have two dots
+				return "", multipleDecimalPoints
+			}
+			decimalFound = true
+		} else if !unicode.IsDigit(c) {
+			return "", fmt.Errorf("unknown numeric character '%c'", c)
+		} else if decimalFound {
+			decPart.WriteRune(c)
+		} else {
+			intPart.WriteRune(c)
+		}
+	}
+
+	result := strings.Builder{}
+	result.Grow(2 * len(number))
+	result.WriteRune(dollar)
+
+	var whole string
+	if intPart.Len() > 0 {
+		whole = intPart.String()
+	} else {
+		whole = "0"
+	}
+
+	comma := false
+	if xtra := len(whole) % 3; xtra != 0 {
+		result.WriteString(whole[0:xtra])
+		whole = whole[xtra:]
+		comma = true
+	}
+
+	for len(whole) > 0 {
+		if comma {
+			result.WriteRune(',')
+		} else {
+			comma = true
+		}
+		result.WriteString(whole[0:3])
+		whole = whole[3:]
+	}
+
+	var dec string
+	result.WriteRune(period)
+	if decimalFound {
+		dec = decPart.String()
+		if len(dec) > 2 {
+			dec = dec[0:2]
+		}
+		result.Write([]byte(dec))
+	}
+	for i := len(dec); i < 2; i++ {
+		result.WriteRune('0')
+	}
+
+	return result.String(), nil
+}
---
+++ text/currency_test.go
@@ -0,0 +1,33 @@
+package text
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFormatUSD(t *testing.T) {
+	testFormatUSD(t, "$1,234,567.89", "1234567.89")
+	testFormatUSD(t, "$234,567.89", "234567.89")
+	testFormatUSD(t, "$34,567.89", "34567.89")
+	testFormatUSD(t, "$4,567.89", "4567.89")
+	testFormatUSD(t, "$567.89", "567.89")
+	testFormatUSD(t, "$67.89", "67.89")
+	testFormatUSD(t, "$7.89", "7.89")
+	testFormatUSD(t, "$0.89", ".89")
+	testFormatUSD(t, "$67.12", "67.127")
+	testFormatUSD(t, "$67.12", "67.123")
+	testFormatUSD(t, "$67.12", "67.12")
+	testFormatUSD(t, "$67.10", "67.1")
+	testFormatUSD(t, "$67.00", "67")
+	testFormatUSD(t, "$7.00", "7")
+	testFormatUSD(t, "$0.00", "")
+	testFormatUSD(t, "$0.00", ".")
+}
+
+func testFormatUSD(t *testing.T, expected, number string) {
+	formatted, err := FormatUSD(number)
+	require.NoError(t, err)
+	assert.Equal(t, expected, formatted)
+}
