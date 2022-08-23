bd24bc13480029d7418d73d458f0684204fd7c6e
mAdkins <software@doorways.org>
2022-08-22 17:03:19 -0700

    Finish error/doc.go changes

--- error/doc.go
+++ error/doc.go
@@ -1,4 +1,5 @@
 // Package error defines some non-standard error mechanisms:
 // - Error with an error with an array of strings representing error details
 // - Error with an error with a map of strings representing error details
+// - Add NotYetImplemented() and ToBeImplemented(name) 'standard' error functions.
 package error
