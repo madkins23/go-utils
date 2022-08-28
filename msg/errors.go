ba908852c18dc5d65b7873329c62c2bc5302808f
mAdkins <software@doorways.org>
2022-08-28 10:08:17 -0700

    Make specific name field visible as Name

--- msg/errors.go
+++ msg/errors.go
@@ -8,15 +8,15 @@ const strBlockedNamed = " is blocked"
 // ErrBlocked is a custom error representing a blocked function or method.
 type ErrBlocked struct {
 	// Optional name of deprecated function or method.
-	name string
+	Name string
 }
 
 // Error implements the predefined error interface.
 func (b *ErrBlocked) Error() string {
-	if b.name == "" {
+	if b.Name == "" {
 		return strBlocked
 	} else {
-		return b.name + strBlockedNamed
+		return b.Name + strBlockedNamed
 	}
 }
 
@@ -28,15 +28,15 @@ const strDeprecatedNamed = " is deprecated"
 // ErrDeprecated is a custom error representing a deprecated function or method.
 type ErrDeprecated struct {
 	// Optional name of deprecated function or method.
-	name string
+	Name string
 }
 
 // Error implements the predefined error interface.
 func (d *ErrDeprecated) Error() string {
-	if d.name == "" {
+	if d.Name == "" {
 		return strDeprecated
 	} else {
-		return d.name + strDeprecatedNamed
+		return d.Name + strDeprecatedNamed
 	}
 }
 
@@ -48,14 +48,14 @@ const strNotImplementedNamed = " is not implemented yet"
 // ErrNotImplemented is a custom error representing an unimplemented function or method.
 type ErrNotImplemented struct {
 	// Optional name of unimplemented function or method.
-	name string
+	Name string
 }
 
 // Error implements the predefined error interface.
 func (ni *ErrNotImplemented) Error() string {
-	if ni.name == "" {
+	if ni.Name == "" {
 		return strNotImplementedYet
 	} else {
-		return ni.name + strNotImplementedNamed
+		return ni.Name + strNotImplementedNamed
 	}
 }
