de0885fcc8bdb1b45a78534eed3712887ce48d4e
mAdkins <software@doorways.org>
2022-08-27 17:10:52 -0700

    Comments for 'standard' errors

--- error/errors.go
+++ error/errors.go
@@ -2,21 +2,24 @@ package error
 
 import "errors"
 
+var notYetImplemented = errors.New("not yet implemented")
+
+// NotYetImplemented returns a generic TBD error.
+func NotYetImplemented() error {
+	return notYetImplemented
+}
+
 const (
 	deprecated      = "deprecated: "
 	toBeImplemented = "to be implemented: "
 )
 
-var notYetImplemented = errors.New("not yet implemented")
-
-func NotYetImplemented() error {
-	return notYetImplemented
-}
-
+// ToBeImplemented returns a TBD error specifying the name of the unimplemented function or method.
 func ToBeImplemented(name string) error {
 	return errors.New(toBeImplemented + name)
 }
 
+// Deprecated returns a deprecation error specifying the name of the deprecated function or method.
 func Deprecated(name string) error {
 	return errors.New(deprecated + name)
 }
