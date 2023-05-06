00f4f5e019d1b1b5396dfa292a21f7fa46f61ae9
mAdkins <software@doorways.org>
2023-05-06 08:00:52 -0700

    Provide a way to modify ConsoleWriter used by ConsoleOrFile after Setup

--- log/console.go
+++ log/console.go
@@ -29,9 +29,23 @@ func Console() {
 // This supports reading the contents from a JSON or YAML configuration file.
 // The struct can also be embedded within a JSON or YAML configuration file.
 type ConsoleOrFile struct {
-	Console bool   `json:"console" yaml:"console"`
+	// True to send output to console (stderr by default) instead of file.
+	Console bool `json:"console" yaml:"console"`
+
+	// True to send console output to stdout instead of stderr
+	UseStdout bool `json:"useStdout" yaml:"useStdout"`
+
+	// Path to logfile to be used if not console mode.
 	LogFile string `json:"logFile" yaml:"logFile"`
-	AsJSON  bool   `json:"asJson" yaml:"asJson"`
+
+	// True to dump data as JSON to logfile,
+	// otherwise console mode output with blank lines when application restarted.
+	AsJSON bool `json:"asJson" yaml:"asJson"`
+
+	// ConsoleWriter object available to change settings or nil if sending JSON to logfile.
+	writer *zerolog.ConsoleWriter
+
+	// Log file object if used or nil if Console is true.
 	logFile *os.File
 }
 
@@ -50,20 +64,32 @@ func (cof *ConsoleOrFile) Setup() error {
 		return time.Now().Local()
 	}
 	if cof.Console {
-		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: "15:04:05"})
+		out := os.Stderr
+		if cof.UseStdout {
+			out = os.Stdout
+		}
+		cof.writer = &zerolog.ConsoleWriter{Out: out, TimeFormat: "15:04:05"}
+		log.Logger = log.Output(cof.writer)
 	} else if cof.logFile, err = os.OpenFile(cof.LogFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666); err != nil {
 		return fmt.Errorf("log file creation: %w", err)
+	} else if cof.AsJSON {
+		log.Logger = log.Output(cof.logFile)
 	} else {
-		if cof.AsJSON {
-			log.Logger = log.Output(cof.logFile)
-		} else {
-			// Separate blocks of log statements for each run.
-			_, _ = fmt.Fprintln(cof.logFile)
-			// Use ConsoleWriter for readable text instead of JSON blocks.
-			log.Logger = log.Output(zerolog.ConsoleWriter{Out: cof.logFile, TimeFormat: "15:04:05", NoColor: true})
-		}
+		// Separate blocks of log statements for each run.
+		_, _ = fmt.Fprintln(cof.logFile)
+		// Use ConsoleWriter for readable text instead of JSON blocks.
+		cof.writer = &zerolog.ConsoleWriter{Out: cof.logFile, TimeFormat: "15:04:05", NoColor: true}
+		log.Output(cof.writer)
 	}
 	return nil
+}
+
+// Writer returns a pointer to the zerolog.ConsoleWriter created in Setup
+// or nil if a log file was configured.
+// This object can be used to derive console loggers with additional
+// configuration later in their usage.
+func (cof *ConsoleOrFile) Writer() *zerolog.ConsoleWriter {
+	return cof.writer
 }
 
 // Close any log file that may have been opened.
--- log/console_test.go
+++ log/console_test.go
@@ -1,15 +1,87 @@
 package log
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
+	"testing"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/stretchr/testify/require"
 )
+
+func TestConsoleOrFile_Setup(t *testing.T) {
+	cof := ConsoleOrFile{
+		Console: true,
+	}
+	require.NoError(t, cof.Setup())
+	cof = ConsoleOrFile{
+		Console: false,
+		LogFile: "/tmp/console-or-file.log",
+		AsJSON:  true,
+	}
+	require.NoError(t, cof.Setup())
+}
 
 func ExampleConsole() {
 	Console()
 	Logger().Info().Msg("Easier to read")
 	// Output:
+}
+
+func ExampleConsoleOrFile_formatExtra() {
+	cof := ConsoleOrFile{
+		Console:   true,
+		UseStdout: true,
+	}
+	if err := cof.Setup(); err != nil {
+		fmt.Printf("Log file creation error: %s\n", err)
+		return
+	}
+	defer cof.CloseForDefer()
+
+	// Get pointer to starting zerolog.ConsoleWriter().
+	writer := cof.Writer()
+	if writer == nil {
+		fmt.Printf("No writer available\n")
+		return
+	}
+	// Make a copy or the changes will persist.
+	fixed := *writer
+	// Make changes to the new ConsoleWriter.
+	// Exclude timestamp so that example output will match.
+	fixed.NoColor = true
+	fixed.PartsExclude = []string{zerolog.TimestampFieldName}
+	fixed.FormatExtra =
+		// This function will add extra stuff to the end of each log line.
+		// The data map passed in contains all the items to be logged by name.
+		func(data map[string]interface{}, buffer *bytes.Buffer) error {
+			// Delete timestamp from data so that example output will match.
+			delete(data, zerolog.TimestampFieldName)
+			if marshaled, err := json.MarshalIndent(data, "", "  "); err != nil {
+				return fmt.Errorf("marshal data into json: %w", err)
+			} else {
+				buffer.WriteString("\n")
+				buffer.Write(marshaled)
+				buffer.WriteString("\n")
+				return nil
+			}
+		}
+	// Make a new log with the fixed ConsoleWriter.
+	myLog := log.Output(fixed)
+	myLog.Info().Str("alpha", "one").Int("bravo", 2).Msg("reversed")
+
+	// Output:
+	// INF reversed alpha=one bravo=2
+	// {
+	//   "alpha": "one",
+	//   "bravo": 2,
+	//   "level": "info",
+	//   "message": "reversed"
+	// }
 }
 
 func ExampleConsoleOrFile() {
