d9af35ae8144de1c76e96510cd43f5d06e7bd9fb
mAdkins <software@doorways.org>
2023-06-26 07:09:05 -0700

    app.Terminator should only Shutdown() once

--- app/terminator.go
+++ app/terminator.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"errors"
+	"sync"
 )
 
 // SubSystem to be shut down by Terminator.
@@ -11,6 +12,8 @@ type SubSystem interface {
 
 // Terminator is used to shut down subsystems gracefully.
 type Terminator struct {
+	lock       sync.Mutex
+	shutDown   bool
 	subSystems []SubSystem
 }
 
@@ -25,9 +28,17 @@ func (t *Terminator) Add(subSystem SubSystem) {
 }
 
 func (t *Terminator) Shutdown() error {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+
+	if t.shutDown {
+		return nil
+	}
+
 	errs := make([]error, len(t.subSystems))
 	for i, subSystem := range t.subSystems {
 		errs[i] = subSystem.Shutdown()
 	}
+	t.shutDown = true
 	return errors.Join(errs...)
 }
